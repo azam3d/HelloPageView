3248403c8fb9ad01a9e4b522025a632053f8d4ed
Azam <swooshfx@gmail.com>
2018-01-21 23:18:32 +0800

    Add Xcode project.

---
+++ HelloPageView/HelloPageView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloPageView
+//
+//  Created by Azam Baderi on 21/01/2018.
+//  Copyright © 2018 Twebble. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
